305f26166c79643b6de8eb7fee64ca4f6fa8e5bb
Alun Jones <alunacjones@gmail.com>
2025-06-07 01:22:30 +0100

    missing doc comments

--- src/LSL.Resources/IJsonResourceReader.cs
+++ src/LSL.Resources/IJsonResourceReader.cs
@@ -2,7 +2,15 @@ using System;
 
 namespace LSL.Resources;
 
+/// <summary>
+/// IJsonResourceReader
+/// </summary>
 public interface IJsonResourceReader
 {
+    /// <summary>
+    /// Read a JSON resource into an instance of <paramref name="type"/>
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
     object ReadJsonResource(Type type);
 }
